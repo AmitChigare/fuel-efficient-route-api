bc403bafa8c78414a1a2d8d6e80fb600d7981b3a
AmitChigare <amit.gokakfalls17@gmail.com>
2023-07-15 12:47:09 +0530

    Final Commit

---
+++ README.md
@@ -0,0 +1,69 @@
+## Repository Setup
+
+1. Clone the repository to your local machine:
+
+   ```
+   $ git clone <repository-url>
+   ```
+
+2. Navigate to the project directory:
+
+   ```
+   $ cd <directory>
+   ```
+
+3. Install the required dependencies:
+
+   ```
+   $ pip install -r requirements.txt
+   ```
+
+4. Set up the database:
+
+   ```
+   $ python manage.py makemigrations
+   $ python manage.py migrate
+   ```
+
+5. Create a superuser to access the admin interface:
+
+   ```
+   $ python manage.py createsuperuser
+   ```
+
+6. Start the development server:
+
+   ```
+   $ python manage.py runserver
+   ```
+
+#### Admin Panel:
+
+- Admin Panel has the functionality to have access over all the Entries and Users. Do check it out to have more control over the api. [Admin Panel](http://localhost:8000/admin)
+
+### Configuration
+
+- Modify the project settings in `settings.py` as needed, such as database settings, static files, etc. If hosted, remember to add the host address in the `setting.py` file.
+
+## Running the Test Suite
+
+- Run the tests to make sure everything is working correctly:
+
+  ```
+  $ python manage.py test
+  ```
+
+  This command will run all the tests and display the results in the console.
+
+## Running the API Server
+
+- Run the following command to start the API server:
+  ```
+  $ python manage.py runserver 8000
+  ```
+  The API server will start, and you can access it at http://localhost:8000/ in your web browser or via API clients like cURL or Postman.
+  Make sure to replace 8000 with the appropriate port number if you have configured a different port for your Django project.
+
+## Deployment
+
+- Refer to the official Django documentation for instructions on deploying Django projects to production environments.
--- schoolProject/schoolApp/views.py
+++ schoolProject/schoolApp/views.py
@@ -8,10 +8,11 @@ from geopy.geocoders import Nominatim
 
 geolocator = Nominatim(user_agent="FuelEfficientRoutesApp")
 
-def fuel_efficient_route2(request, pincode, latitude, longitude):
+
+def fuel_efficient_route(request, pincode, latitude, longitude):
     origin = (latitude, longitude)
-    if latitude==0.0 and longitude==0.0:
-        origin_name='Location Unavailable'
+    if latitude == 0.0 and longitude == 0.0:
+        origin_name = "Location Unavailable"
     else:
         origin_name = get_location_name(latitude, longitude)
 
@@ -25,30 +26,52 @@ def fuel_efficient_route2(request, pincode, latitude, longitude):
 
     distances.sort(key=lambda x: x[1])
 
-    #Pagination
-    paginator=Paginator(distances, 10)
-    page_number=request.GET.get('page')
-    page_obj=paginator.get_page(page_number)
+    # Pagination
+    paginator = Paginator(distances, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
         "pincode": pincode,
         "origin_name": origin_name,
         "page_obj": page_obj,
     }
-    return render(request, "fuel_efficient_route.html", context) 
+    return render(request, "fuel_efficient_route.html", context)
 
-def fuel_efficient_route(request):
+
+def home(request):
+    # pin_codes = []
+    # for i in range(150):
+    #     if i % 40 == 0:
+    #         # Generate a new random pin code if a multiple of 10 is encountered
+    #         pin_code = str(random.randint(100000, 999999))
+    #     pin_codes.append(pin_code)
+
+    # for pin_code in pin_codes:
+    #     school = {
+    #         "name": f"School Name {random.randint(99, 500000)} Some Random {random.randint(1, 500)}",
+    #         "address": f"Address {random.randint(1, 500)} AGY {random.randint(1, 500)}FUG{random.randint(1, 500)}WI IWO{random.randint(1, 500)}JOIJU UIHUFIE{random.randint(1, 500)}IOJEFFE ",
+    #         "pin_code": pin_code,
+    #         "latitude": random.uniform(-90, 90),
+    #         "longitude": random.uniform(-180, 180),
+    #     }
+    #     School.objects.create(**school)
     if request.method == "POST":
         pincode = request.POST.get("pincode")
         latitude = request.POST.get("latitude")
         longitude = request.POST.get("longitude")
         print(latitude, longitude)
         if not latitude or not longitude:
-            latitude=0.0
-            longitude=0.0
-        return redirect('fuel_efficient_route2', pincode=pincode, latitude=latitude, longitude=longitude)
+            latitude = 0.0
+            longitude = 0.0
+        return redirect(
+            "fuel_efficient_route",
+            pincode=pincode,
+            latitude=latitude,
+            longitude=longitude,
+        )
 
-    return render(request, "fuel_efficient_form.html")
+    return render(request, "home.html")
 
 
 def get_location_name(latitude, longitude):
